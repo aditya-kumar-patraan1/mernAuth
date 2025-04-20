fa4c266c58db6c0c849e4f4a9eaace283d7ac691
Aditya Kumar Rajput <pythonboot06@gmail.com>
2025-04-20 11:00:39 +0530

    Update Navbar.jsx

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-
-    const Navigate = useNavigate();
+  const Navigate = useNavigate();
 
   return (
     <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
       {/* Logo */}
-      <a className="text-2xl font-semibold text-gray-800 hover:cursor-pointer" href="/">
+      <NavLink
+        to="/"
+        className="text-2xl font-semibold text-gray-800 hover:cursor-pointer"
+      >
         MyLogo
-      </a>
+      </NavLink>
 
       {/* Links */}
       <div className="flex items-center gap-6">
-        <a href="#contact" className="text-gray-700 hover:text-blue-600 transition">Contact</a>
-        <a href="#about" className="text-gray-700 hover:text-blue-600 transition">About</a>
-        <a href="#help" className="text-gray-700 hover:text-blue-600 transition">Help</a>
+        <NavLink
+          to="/contact"
+          className="text-gray-700 hover:text-blue-600 transition"
+        >
+          Contact
+        </NavLink>
+        <NavLink
+          to="/about"
+          className="text-gray-700 hover:text-blue-600 transition"
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/help"
+          className="text-gray-700 hover:text-blue-600 transition"
+        >
+          Help
+        </NavLink>
       </div>
 
       {/* Buttons */}
       <div className="flex items-center gap-4">
-        <button className="text-gray-700 hover:text-blue-600 transition hover:cursor-pointer" onClick={()=>Navigate("/LoginPage")}>Login</button>
-        <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-800 transition hover:cursor-pointer"  onClick={()=>Navigate("/RegisterPage")}>
+        <button
+          className="text-gray-700 hover:text-blue-600 transition hover:cursor-pointer"
+          onClick={() => Navigate("/LoginPage")}
+        >
+          Login
+        </button>
+        <button
+          className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-800 transition hover:cursor-pointer"
+          onClick={() => Navigate("/RegisterPage")}
+        >
           Register
         </button>
       </div>
